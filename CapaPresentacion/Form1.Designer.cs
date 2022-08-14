266d0dd68a1f27535aa890d63dc127299367ae33
ManuelBS96 <manuelbns60@gmail.com>
2022-08-14 18:50:10 -0500

    Se crean las capas y la interfaz Form1 con botones y campos para los productos

--- CapaPresentacion/Form1.Designer.cs
+++ CapaPresentacion/Form1.Designer.cs
@@ -29,86 +29,86 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.Inventario = new System.Windows.Forms.GroupBox();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.LblArticulo = new System.Windows.Forms.Label();
+            this.LblMarca = new System.Windows.Forms.Label();
+            this.LblDescrip = new System.Windows.Forms.Label();
+            this.LblPrecio = new System.Windows.Forms.Label();
+            this.LblStock = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
+            this.Inventario.SuspendLayout();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.Inventario);
             this.panel1.Controls.Add(this.groupBox1);
             this.panel1.Location = new System.Drawing.Point(3, 1);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1000, 490);
             this.panel1.TabIndex = 0;
             // 
-            // groupBox2
+            // Inventario
             // 
-            this.groupBox2.Controls.Add(this.dataGridView1);
-            this.groupBox2.Controls.Add(this.button3);
-            this.groupBox2.Controls.Add(this.button2);
-            this.groupBox2.Location = new System.Drawing.Point(392, 15);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(588, 462);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "groupBox2";
+            this.Inventario.Controls.Add(this.dataGridView1);
+            this.Inventario.Controls.Add(this.btnEditar);
+            this.Inventario.Controls.Add(this.btnEliminar);
+            this.Inventario.Location = new System.Drawing.Point(392, 15);
+            this.Inventario.Name = "Inventario";
+            this.Inventario.Size = new System.Drawing.Size(588, 462);
+            this.Inventario.TabIndex = 1;
+            this.Inventario.TabStop = false;
+            this.Inventario.Text = "Inventario";
             // 
-            // button3
+            // btnEditar
             // 
-            this.button3.Location = new System.Drawing.Point(143, 23);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(120, 24);
-            this.button3.TabIndex = 1;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnEditar.Location = new System.Drawing.Point(143, 31);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(120, 44);
+            this.btnEditar.TabIndex = 1;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnEliminar
             // 
-            this.button2.Location = new System.Drawing.Point(17, 23);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(120, 24);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnEliminar.Location = new System.Drawing.Point(17, 31);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(120, 44);
+            this.btnEliminar.TabIndex = 0;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.LblStock);
+            this.groupBox1.Controls.Add(this.LblPrecio);
+            this.groupBox1.Controls.Add(this.LblDescrip);
+            this.groupBox1.Controls.Add(this.LblMarca);
+            this.groupBox1.Controls.Add(this.LblArticulo);
             this.groupBox1.Controls.Add(this.textBox5);
             this.groupBox1.Controls.Add(this.textBox4);
             this.groupBox1.Controls.Add(this.textBox3);
             this.groupBox1.Controls.Add(this.textBox2);
             this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btnAgregar);
             this.groupBox1.Location = new System.Drawing.Point(21, 15);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(356, 462);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "Datos del articulo";
             // 
             // textBox5
             // 
@@ -146,69 +146,69 @@
             this.textBox1.Size = new System.Drawing.Size(181, 22);
             this.textBox1.TabIndex = 1;
             // 
-            // button1
+            // btnAgregar
             // 
-            this.button1.Location = new System.Drawing.Point(161, 254);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(143, 37);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAgregar.Location = new System.Drawing.Point(190, 250);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(143, 37);
+            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // LblArticulo
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 31);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.LblArticulo.AutoSize = true;
+            this.LblArticulo.Location = new System.Drawing.Point(6, 31);
+            this.LblArticulo.Name = "LblArticulo";
+            this.LblArticulo.Size = new System.Drawing.Size(51, 16);
+            this.LblArticulo.TabIndex = 6;
+            this.LblArticulo.Text = "Articulo";
+            this.LblArticulo.Click += new System.EventHandler(this.label1_Click);
             // 
-            // label2
+            // LblMarca
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 68);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "label2";
+            this.LblMarca.AutoSize = true;
+            this.LblMarca.Location = new System.Drawing.Point(6, 111);
+            this.LblMarca.Name = "LblMarca";
+            this.LblMarca.Size = new System.Drawing.Size(45, 16);
+            this.LblMarca.TabIndex = 7;
+            this.LblMarca.Text = "Marca";
             // 
-            // label3
+            // LblDescrip
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 111);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 16);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "label3";
+            this.LblDescrip.AutoSize = true;
+            this.LblDescrip.Location = new System.Drawing.Point(6, 65);
+            this.LblDescrip.Name = "LblDescrip";
+            this.LblDescrip.Size = new System.Drawing.Size(79, 16);
+            this.LblDescrip.TabIndex = 8;
+            this.LblDescrip.Text = "Descripcion";
             // 
-            // label4
+            // LblPrecio
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 150);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(44, 16);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "label4";
+            this.LblPrecio.AutoSize = true;
+            this.LblPrecio.Location = new System.Drawing.Point(6, 150);
+            this.LblPrecio.Name = "LblPrecio";
+            this.LblPrecio.Size = new System.Drawing.Size(56, 16);
+            this.LblPrecio.TabIndex = 9;
+            this.LblPrecio.Text = "Precio $";
             // 
-            // label5
+            // LblStock
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 190);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(44, 16);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "label5";
+            this.LblStock.AutoSize = true;
+            this.LblStock.Location = new System.Drawing.Point(6, 190);
+            this.LblStock.Name = "LblStock";
+            this.LblStock.Size = new System.Drawing.Size(41, 16);
+            this.LblStock.TabIndex = 10;
+            this.LblStock.Text = "Stock";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(17, 53);
+            this.dataGridView1.Location = new System.Drawing.Point(17, 81);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(555, 392);
+            this.dataGridView1.Size = new System.Drawing.Size(555, 364);
             this.dataGridView1.TabIndex = 2;
             // 
             // Form1
@@ -220,7 +220,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
-            this.groupBox2.ResumeLayout(false);
+            this.Inventario.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -231,21 +231,21 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox Inventario;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnEliminar;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label LblStock;
+        private System.Windows.Forms.Label LblPrecio;
+        private System.Windows.Forms.Label LblDescrip;
+        private System.Windows.Forms.Label LblMarca;
+        private System.Windows.Forms.Label LblArticulo;
         private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
