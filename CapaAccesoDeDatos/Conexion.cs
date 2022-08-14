7b64cd3dcea379aa9d147b4036bff58ec5af89cc
ManuelBS96 <manuelbns60@gmail.com>
2022-08-14 18:20:51 -0500

    Agregar archivos de proyecto.

---
+++ CapaAccesoDeDatos/Conexion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CapaAccesoDeDatos
+{
+    
+    public class Conexion
+    {
+
+        private SqlConnection Conn = new SqlConnection("Server= localhost;DataBase = CRUDbasico; Integrated Security = true");
+        public SqlConnection AbrirConexion()
+        {
+            if (Conn.State == ConnectionState.Closed)
+                Conn.Open();
+            return Conn;
+        }
+        public SqlConnection CerrarConexion()
+        {
+            if (Conn.State == ConnectionState.Open)
+                Conn.Close();
+            return Conn;
+        }
+    }
+}
