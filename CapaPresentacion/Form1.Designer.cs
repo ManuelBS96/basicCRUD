0bed131bfbe54f87fb3cee9c14d397b6c8e845e8
ManuelBS96 <manuelbns60@gmail.com>
2022-09-11 20:32:38 -0500

    Se cargan los articulos en el formulario

--- CapaPresentacion/Form1.Designer.cs
+++ CapaPresentacion/Form1.Designer.cs
@@ -29,200 +29,185 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.Inventario = new System.Windows.Forms.GroupBox();
-            this.btnEditar = new System.Windows.Forms.Button();
-            this.btnEliminar = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.btnAgregar = new System.Windows.Forms.Button();
-            this.LblArticulo = new System.Windows.Forms.Label();
-            this.LblMarca = new System.Windows.Forms.Label();
-            this.LblDescrip = new System.Windows.Forms.Label();
-            this.LblPrecio = new System.Windows.Forms.Label();
             this.LblStock = new System.Windows.Forms.Label();
+            this.LblPrecio = new System.Windows.Forms.Label();
+            this.LblMarca = new System.Windows.Forms.Label();
+            this.LblDescripcion = new System.Windows.Forms.Label();
+            this.LblNombre = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.textDescripcion = new System.Windows.Forms.TextBox();
+            this.textMarca = new System.Windows.Forms.TextBox();
+            this.textPrecio = new System.Windows.Forms.TextBox();
+            this.textStock = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.LblEliminar = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
-            this.Inventario.SuspendLayout();
-            this.groupBox1.SuspendLayout();
+            this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.Inventario);
-            this.panel1.Controls.Add(this.groupBox1);
-            this.panel1.Location = new System.Drawing.Point(3, 1);
+            this.panel1.Controls.Add(this.btnGuardar);
+            this.panel1.Controls.Add(this.textStock);
+            this.panel1.Controls.Add(this.textPrecio);
+            this.panel1.Controls.Add(this.textMarca);
+            this.panel1.Controls.Add(this.textDescripcion);
+            this.panel1.Controls.Add(this.textNombre);
+            this.panel1.Controls.Add(this.LblStock);
+            this.panel1.Controls.Add(this.LblPrecio);
+            this.panel1.Controls.Add(this.LblMarca);
+            this.panel1.Controls.Add(this.LblDescripcion);
+            this.panel1.Controls.Add(this.LblNombre);
+            this.panel1.Location = new System.Drawing.Point(8, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1000, 490);
+            this.panel1.Size = new System.Drawing.Size(306, 439);
             this.panel1.TabIndex = 0;
-            // 
-            // Inventario
-            // 
-            this.Inventario.Controls.Add(this.dataGridView1);
-            this.Inventario.Controls.Add(this.btnEditar);
-            this.Inventario.Controls.Add(this.btnEliminar);
-            this.Inventario.Location = new System.Drawing.Point(392, 15);
-            this.Inventario.Name = "Inventario";
-            this.Inventario.Size = new System.Drawing.Size(588, 462);
-            this.Inventario.TabIndex = 1;
-            this.Inventario.TabStop = false;
-            this.Inventario.Text = "Inventario";
-            // 
-            // btnEditar
-            // 
-            this.btnEditar.Location = new System.Drawing.Point(143, 31);
-            this.btnEditar.Name = "btnEditar";
-            this.btnEditar.Size = new System.Drawing.Size(120, 44);
-            this.btnEditar.TabIndex = 1;
-            this.btnEditar.Text = "Editar";
-            this.btnEditar.UseVisualStyleBackColor = true;
-            // 
-            // btnEliminar
-            // 
-            this.btnEliminar.Location = new System.Drawing.Point(17, 31);
-            this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(120, 44);
-            this.btnEliminar.TabIndex = 0;
-            this.btnEliminar.Text = "Eliminar";
-            this.btnEliminar.UseVisualStyleBackColor = true;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.LblStock);
-            this.groupBox1.Controls.Add(this.LblPrecio);
-            this.groupBox1.Controls.Add(this.LblDescrip);
-            this.groupBox1.Controls.Add(this.LblMarca);
-            this.groupBox1.Controls.Add(this.LblArticulo);
-            this.groupBox1.Controls.Add(this.textBox5);
-            this.groupBox1.Controls.Add(this.textBox4);
-            this.groupBox1.Controls.Add(this.textBox3);
-            this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Controls.Add(this.btnAgregar);
-            this.groupBox1.Location = new System.Drawing.Point(21, 15);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(356, 462);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Datos del articulo";
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(152, 65);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(181, 22);
-            this.textBox5.TabIndex = 5;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(152, 111);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(181, 22);
-            this.textBox4.TabIndex = 4;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(152, 187);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(181, 22);
-            this.textBox3.TabIndex = 3;
-            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(152, 150);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(181, 22);
-            this.textBox2.TabIndex = 2;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(152, 25);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(181, 22);
-            this.textBox1.TabIndex = 1;
-            // 
-            // btnAgregar
-            // 
-            this.btnAgregar.Location = new System.Drawing.Point(190, 250);
-            this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(143, 37);
-            this.btnAgregar.TabIndex = 0;
-            this.btnAgregar.Text = "Agregar";
-            this.btnAgregar.UseVisualStyleBackColor = true;
-            // 
-            // LblArticulo
-            // 
-            this.LblArticulo.AutoSize = true;
-            this.LblArticulo.Location = new System.Drawing.Point(6, 31);
-            this.LblArticulo.Name = "LblArticulo";
-            this.LblArticulo.Size = new System.Drawing.Size(51, 16);
-            this.LblArticulo.TabIndex = 6;
-            this.LblArticulo.Text = "Articulo";
-            this.LblArticulo.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // LblMarca
-            // 
-            this.LblMarca.AutoSize = true;
-            this.LblMarca.Location = new System.Drawing.Point(6, 111);
-            this.LblMarca.Name = "LblMarca";
-            this.LblMarca.Size = new System.Drawing.Size(45, 16);
-            this.LblMarca.TabIndex = 7;
-            this.LblMarca.Text = "Marca";
-            // 
-            // LblDescrip
-            // 
-            this.LblDescrip.AutoSize = true;
-            this.LblDescrip.Location = new System.Drawing.Point(6, 65);
-            this.LblDescrip.Name = "LblDescrip";
-            this.LblDescrip.Size = new System.Drawing.Size(79, 16);
-            this.LblDescrip.TabIndex = 8;
-            this.LblDescrip.Text = "Descripcion";
-            // 
-            // LblPrecio
-            // 
-            this.LblPrecio.AutoSize = true;
-            this.LblPrecio.Location = new System.Drawing.Point(6, 150);
-            this.LblPrecio.Name = "LblPrecio";
-            this.LblPrecio.Size = new System.Drawing.Size(56, 16);
-            this.LblPrecio.TabIndex = 9;
-            this.LblPrecio.Text = "Precio $";
             // 
             // LblStock
             // 
             this.LblStock.AutoSize = true;
-            this.LblStock.Location = new System.Drawing.Point(6, 190);
+            this.LblStock.Location = new System.Drawing.Point(7, 173);
             this.LblStock.Name = "LblStock";
             this.LblStock.Size = new System.Drawing.Size(41, 16);
-            this.LblStock.TabIndex = 10;
+            this.LblStock.TabIndex = 4;
             this.LblStock.Text = "Stock";
+            // 
+            // LblPrecio
+            // 
+            this.LblPrecio.AutoSize = true;
+            this.LblPrecio.Location = new System.Drawing.Point(7, 127);
+            this.LblPrecio.Name = "LblPrecio";
+            this.LblPrecio.Size = new System.Drawing.Size(46, 16);
+            this.LblPrecio.TabIndex = 3;
+            this.LblPrecio.Text = "Precio";
+            // 
+            // LblMarca
+            // 
+            this.LblMarca.AutoSize = true;
+            this.LblMarca.Location = new System.Drawing.Point(7, 93);
+            this.LblMarca.Name = "LblMarca";
+            this.LblMarca.Size = new System.Drawing.Size(45, 16);
+            this.LblMarca.TabIndex = 2;
+            this.LblMarca.Text = "Marca";
+            // 
+            // LblDescripcion
+            // 
+            this.LblDescripcion.AutoSize = true;
+            this.LblDescripcion.Location = new System.Drawing.Point(7, 53);
+            this.LblDescripcion.Name = "LblDescripcion";
+            this.LblDescripcion.Size = new System.Drawing.Size(79, 16);
+            this.LblDescripcion.TabIndex = 1;
+            this.LblDescripcion.Text = "Descripcion";
+            // 
+            // LblNombre
+            // 
+            this.LblNombre.AutoSize = true;
+            this.LblNombre.Location = new System.Drawing.Point(7, 20);
+            this.LblNombre.Name = "LblNombre";
+            this.LblNombre.Size = new System.Drawing.Size(56, 16);
+            this.LblNombre.TabIndex = 0;
+            this.LblNombre.Text = "Nombre";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.LblEliminar);
+            this.panel2.Controls.Add(this.btnEditar);
+            this.panel2.Location = new System.Drawing.Point(320, 5);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(633, 439);
+            this.panel2.TabIndex = 1;
+            // 
+            // textNombre
+            // 
+            this.textNombre.Location = new System.Drawing.Point(88, 14);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(212, 22);
+            this.textNombre.TabIndex = 5;
+            // 
+            // textDescripcion
+            // 
+            this.textDescripcion.Location = new System.Drawing.Point(88, 53);
+            this.textDescripcion.Name = "textDescripcion";
+            this.textDescripcion.Size = new System.Drawing.Size(212, 22);
+            this.textDescripcion.TabIndex = 6;
+            // 
+            // textMarca
+            // 
+            this.textMarca.Location = new System.Drawing.Point(88, 93);
+            this.textMarca.Name = "textMarca";
+            this.textMarca.Size = new System.Drawing.Size(212, 22);
+            this.textMarca.TabIndex = 7;
+            this.textMarca.TextChanged += new System.EventHandler(this.textMarca_TextChanged);
+            // 
+            // textPrecio
+            // 
+            this.textPrecio.Location = new System.Drawing.Point(88, 127);
+            this.textPrecio.Name = "textPrecio";
+            this.textPrecio.Size = new System.Drawing.Size(212, 22);
+            this.textPrecio.TabIndex = 8;
+            // 
+            // textStock
+            // 
+            this.textStock.Location = new System.Drawing.Point(88, 173);
+            this.textStock.Name = "textStock";
+            this.textStock.Size = new System.Drawing.Size(212, 22);
+            this.textStock.TabIndex = 9;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(149, 221);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(151, 27);
+            this.btnGuardar.TabIndex = 10;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(3, 14);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(151, 27);
+            this.btnEditar.TabIndex = 11;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // LblEliminar
+            // 
+            this.LblEliminar.Location = new System.Drawing.Point(169, 14);
+            this.LblEliminar.Name = "LblEliminar";
+            this.LblEliminar.Size = new System.Drawing.Size(151, 27);
+            this.LblEliminar.TabIndex = 12;
+            this.LblEliminar.Text = "Eliminar";
+            this.LblEliminar.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(17, 81);
+            this.dataGridView1.Location = new System.Drawing.Point(323, 57);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(555, 364);
-            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.Size = new System.Drawing.Size(627, 381);
+            this.dataGridView1.TabIndex = 13;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1010, 491);
+            this.ClientSize = new System.Drawing.Size(965, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
-            this.Inventario.ResumeLayout(false);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
@@ -231,21 +216,20 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.GroupBox Inventario;
-        private System.Windows.Forms.Button btnEditar;
-        private System.Windows.Forms.Button btnEliminar;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.Label LblStock;
         private System.Windows.Forms.Label LblPrecio;
-        private System.Windows.Forms.Label LblDescrip;
         private System.Windows.Forms.Label LblMarca;
-        private System.Windows.Forms.Label LblArticulo;
+        private System.Windows.Forms.Label LblDescripcion;
+        private System.Windows.Forms.Label LblNombre;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.TextBox textStock;
+        private System.Windows.Forms.TextBox textPrecio;
+        private System.Windows.Forms.TextBox textMarca;
+        private System.Windows.Forms.TextBox textDescripcion;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.Button LblEliminar;
+        private System.Windows.Forms.Button btnEditar;
         private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- CapaPresentacion/Form1.cs
+++ CapaPresentacion/Form1.cs
@@ -8,21 +8,102 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using CapaNegocio;
+
 namespace CapaPresentacion
 {
     public partial class Form1 : Form
     {
+
+        CN_Productos objetoCN = new CN_Productos();
+        private string idProducto = null;
+        private bool Editar = false;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void textBox3_TextChanged(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-
+            MostrarProdctos();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void MostrarProdctos()
+        {
+            CN_Productos objeto = new CN_Productos();
+            dataGridView1.DataSource = objeto.MostrarProd();
+        }
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            //INSERTAR
+            if (Editar == false)
+            {
+                try
+                {
+                    objetoCN.InsertarPRod(textNombre.Text, textDescripcion.Text, textMarca.Text, textPrecio.Text, textStock.Text);
+                    MessageBox.Show("se inserto correctamente");
+                    MostrarProdctos();
+                    limpiarForm();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("no se pudo insertar los datos por: " + ex);
+                }
+            }
+            //EDITAR
+            if (Editar == true)
+            {
+                try
+                {
+                    objetoCN.EditarProd(textNombre.Text, textDescripcion.Text, textMarca.Text, textPrecio.Text, textStock.Text, idProducto);
+                    MessageBox.Show("se edito correctamente");
+                    MostrarProdctos();
+                    limpiarForm();
+                    Editar = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("no se pudo editar los datos por: " + ex);
+                }
+            }
+        }
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                Editar = true;
+                textNombre.Text = dataGridView1.CurrentRow.Cells["Nombre"].Value.ToString();
+                textMarca.Text = dataGridView1.CurrentRow.Cells["Marca"].Value.ToString();
+                textDescripcion.Text = dataGridView1.CurrentRow.Cells["Descripcion"].Value.ToString();
+                textPrecio.Text = dataGridView1.CurrentRow.Cells["Precio"].Value.ToString();
+                textStock.Text = dataGridView1.CurrentRow.Cells["Stock"].Value.ToString();
+                idProducto = dataGridView1.CurrentRow.Cells["Id"].Value.ToString();
+            }
+            else
+                MessageBox.Show("seleccione una fila por favor");
+        }
+        private void limpiarForm()
+        {
+            textDescripcion.Clear();
+            textMarca.Text = "";
+            textPrecio.Clear();
+            textStock.Clear();
+            textNombre.Clear();
+        }
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                idProducto = dataGridView1.CurrentRow.Cells["Id"].Value.ToString();
+                objetoCN.EliminarPRod(idProducto);
+                MessageBox.Show("Eliminado correctamente");
+                MostrarProdctos();
+            }
+            else
+                MessageBox.Show("seleccione una fila por favor");
+        }
+
+        private void textMarca_TextChanged(object sender, EventArgs e)
         {
 
         }
